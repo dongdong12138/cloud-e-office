eb17a5d9517b9eacc4d2b9d1b0f224451b7ae19c
BarryDong <286084845@qq.com>
2022-05-06 01:08:11 +0800

    处理登录事件

--- src/views/TheLogin.vue
+++ src/views/TheLogin.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="login">
-    <el-form :model="loginForm" status-icon class="form">
+    <el-form :model="loginForm" :rules="rules" ref="form" class="form">
       <h3>系统登录</h3>
-      <el-form-item>
+      <el-form-item prop="username">
         <el-input type="text" v-model="loginForm.username" placeholder="请输入用户名" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="password">
         <el-input type="password" v-model="loginForm.password" placeholder="请输入密码" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item>
-        <el-input class="input_code" type="text" v-model="loginForm.code" placeholder="请输入验证码" autocomplete="off"></el-input>
+      <el-form-item prop="code">
+        <el-input class="input_code" type="text" v-model="loginForm.code" placeholder="请输入验证码"
+                  autocomplete="off"></el-input>
         <img :src="codeImgUrl" alt="验证码">
       </el-form-item>
       <el-form-item>
         <el-checkbox v-model="rememberMe">记住我</el-checkbox>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" class="btn_submit">登录</el-button>
+        <el-button @click="submitForm" type="primary" class="btn_submit">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -33,8 +34,25 @@ export default {
         code: '',
       },
       codeImgUrl: '',
-      rememberMe: false
+      rememberMe: false,
+      rules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+        code: [{ required: true, message: '请输入验证码', trigger: 'blur' }],
+      }
     }
+  },
+  methods: {
+    submitForm() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          this.$message.error('请输入所有字段！')
+          return false
+        }
+      })
+    },
   }
 }
 </script>
